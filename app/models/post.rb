51360e7ac8906e6cd7fa64bd8a551026a6c36bf3
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-16 16:50:05 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :is_clickbait
+
+  @@clickbait = [/Won't Believe/i, /Secret/i, /Top [0-0]/i, /guess/i]
+
+  def is_clickbait
+    if !@@clickbait.any? {| word | word.match title}
+      errors.add(:title, "not clickbait")
+    end
+  end
 end
