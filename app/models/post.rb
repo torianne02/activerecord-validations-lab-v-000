b0377233c639c583100f9cee0ef416239903133d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-16 16:51:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :is_clickbait
 
   @@clickbait = [/Won't Believe/i, /Secret/i, /Top [0-0]/i, /guess/i]
